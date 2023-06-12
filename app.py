83e5649c1c3a04e9599e8b69b49e7269c75e91f7
nkulakow <01169201@pw.edu.pl>
2023-06-12 19:43:45 +0200

    gui cd

--- app.py
+++ app.py
@@ -13,15 +13,15 @@ database = DatabaseManager(bd_info[0], bd_info[1], bd_info[2])
 def view_pracownicy():
     rows = database.get_all_pracownicy()
     try:
-        pharse = request.form['fraza']
+        phrase = request.form['fraza']
     except BadRequestKeyError:
         pass
     try:
-        rows = filter_table_by_phrase(rows, pharse)
+        rows = filter_table_by_phrase(rows, phrase)
         return render_template('viewpracownicy.html',
                                rows=rows,
                                the_title="Pracownicy",
-                               fraza=pharse)
+                               fraza=phrase)
     except UnboundLocalError:
         return render_template('viewpracownicy.html',
                                rows=rows,
@@ -53,8 +53,11 @@ def delete_pracownik():
 
 @app.route('/gotoaddpracownik', methods=['GET'])
 def go_to_addpracownik():
-    # @TODO wysłać na tą stronę rzeczy takie jak: pary id_stanowiska i jego nazwa
-    return render_template('addpracownik.html', towns=[['(1, 1)', 'Warszawa, pow. warszawski'], ['(3,1)', 'Otwock, pow. otwocki']])
+    # @TODO wysłać na tą stronę towns i positions jak niżej
+    demo_positions = [["1", "Asystent, niekadrowy"], ["2", "Manager, kadrowy"]]
+    return render_template('addpracownik.html',
+                           towns=[['(1, 1)', 'Warszawa, pow. warszawski'], ['(3,1)', 'Otwock, pow. otwocki']],
+                           positions=demo_positions)
 
 
 @app.route('/addpracownik', methods=['POST'])
@@ -63,25 +66,79 @@ def addpracownik():
     return redirect('/entry')
 
 
-@app.route('/givebenefittoppracownik', methods=['POST'] )
+@app.route('/givebenefittoppracownik', methods=['POST'])
 def give_benefit():
-    # @TODO give
+    # @TODO dać benefit wybrany z modifypracownicy.html
     return redirect('/entry')
 
 
 @app.route('/modifyowndata', methods=['POST', 'GET'])
 def go_to_modify_own_data():
-    # @TODO wysłać na tą stronę rzeczy takie jak: pary id_stanowiska i jego nazwa
-    return render_template('modifyowndata.html', id=database.user_id)
+    # @TODO dane postawowe z bd usera trzeba wysłać może być w postaci jak demo niżej
+    demo_dane = ["id", "Imię", "Nazwisko", "Ulica", "Numer domu", "Numer mieszkania", "PESEL", "mail służbowy",
+                 "id powiatu", "id miejscowości", "hasło"]
+    # Tylko jeśli jako tabelka to w tej kolejności koniecznie, można też przekazać poprzez oddzielne zmienne
+    return render_template('modifyowndata.html', data=demo_dane)
 
 
 @app.route('/additionalcontactdetails', methods=['POST', 'GET'])
 def go_to_additional_contact_details():
+    # @TODO zaimplementować pobieranie dodatkowych danych kontaktowych usera z bd, może być jak wyżej
     return render_template('additionalcontactdetails.html', id=database.user_id)
 
 
 @app.route('/saveadditionalcontactdetails', methods=['POST'])
 def modify_additional_contact_details():
+    # @TODO Zaimplementować dodawanie danych kontaktowych do pracownika o danych z additionalcontactdetails.html
+    return redirect('/simpleentry')
+
+
+@app.route('/addcertificatetoself', methods=['POST', 'GET'])
+def go_to_add_certificate():
+    # @TODO Pobieranie już dostępnych certyfikatów z bd i utworzenie jakieś tabelki [[str1, str2]] jak niżej
+    demo_certs = [['0', 'certyfikat 1 jakis tam'], ['1', 'certyfikat inny i opis']]
+    return render_template('addcertificate.html', certificates=demo_certs)
+
+
+@app.route('/createnewcertificate', methods=['POST'])
+def add_new_certificate_to_db():
+    # @TODO Zaimplementować dodawanie certyfikatu do bazy danych a potem do pracownika o danych z addcertificate.html
+    return redirect('/simpleentry')
+
+
+@app.route('/addcertficatetopracownik', methods=['POST'])
+def add_certificate_to_pracownik():
+    # @TODO Zaimplementować dodawanie certyfikatu do pracownika o danych z addcertificate.html
+    return redirect('/simpleentry')
+
+
+@app.route('/manageselfapplications', methods=['POST', 'GET'])
+def go_to_manage_self_applications():
+    demo_wnioski_urlop = [[1, '11/02/2023', 'zalacznik.docx', 'R', '21/08/2023', '25/08/2023', 'zdrowotny']]
+    demo_wnioski_bonus = [[103, '22/05/2022', 'aaaa.pdf', 'Z', 100, 1]]
+    demo_wnioski_inne = [[]]
+    # @TODO Zaimplementować dodawanie wniosków do gui jak wyżej przykład jest
+    return render_template('manageselfapplications.html',
+                           wnioski_urlop=demo_wnioski_urlop,
+                           wnioski_bonus=demo_wnioski_bonus,
+                           wnioski_inne=demo_wnioski_inne)
+
+
+@app.route('/applyforvacation', methods=['POST', 'GET'])
+def apply_for_vacation():
+    # @TODO dodać wniosek o urlop o danych z manageselfapplications.html do bd
+    return redirect('/simpleentry')
+
+
+@app.route('/applyforbonus', methods=['POST', 'GET'])
+def apply_for_bonus():
+    # @TODO dodać wniosek o bonus o danych z manageselfapplications.html do bd
+    return redirect('/simpleentry')
+
+
+@app.route('/applyforsthelse', methods=['POST', 'GET'])
+def apply_for_sth_else():
+    # @TODO dodać wniosek inny o danych z manageselfapplications.html do bd
     return redirect('/simpleentry')
 
 
@@ -89,6 +146,11 @@ def modify_additional_contact_details():
 def login_check():
     login = request.form['login']
     password = request.form['password']
+    # @TODO
+    """
+    zaimplementować inaczej check login - musi sprawdzić czy jest kadrowym czy nie, 
+    też imo by sie przydało zapisać imię i nazwisko do bd (mi z gui będzie łatwiej)
+    """
     if database.check_login(login, password):
         if login == 'jkowalski':
             return redirect('/entry')
